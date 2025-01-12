9c9f25759b8bd605a160a14ab87d0153439e1aaf
Tachera Sasi <tacherasasi@gmail.com>
2025-01-12 13:34:51 +0300

    lang rules and example codes

--- main.py
+++ main.py
@@ -10,19 +10,16 @@ def getFileContent(file_path):
         print(f"File {file_path} does not exist")
     except Exception as e:
         print(f"An error occurred while reading the file: {file_path} ERR:{e}")
-        
-        
+
+
 if len(args) < 2:
     print("Usage: python main.py <file>")
     sys.exit(1)
 else:
     file_name = (args[1])
-    if  file_name.endswith(".ai"):
+    if  file_name.endswith(".vint"):
         content = getFileContent(file_name)
         print(content)
         sys.exit(1)
-        
+
     print("The file should end with '.ai'")
-    
-    
-               

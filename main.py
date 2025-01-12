8a1bdb298c1cf79289a96c2350c598546eb640c7
Tachera Sasi <tacherasasi@gmail.com>
2025-01-12 13:15:42 +0300

    getting the file content

--- main.py
+++ main.py
@@ -2,13 +2,6 @@ import sys
 
 args = sys.argv
 
-if len(args) < 2:
-    print("Usage: python main.py <file>")
-    sys.exit(1)
-else:
-    print(args)
-    
-           
 def getFileContent(file_path):
     try:
         with open(file_path,'r') as file:
@@ -17,3 +10,17 @@ def getFileContent(file_path):
         print(f"File {file_path} does not exist")
     except Exception as e:
         print(f"An error occurred while reading the file: {file_path} ERR:{e}")
+        
+        
+if len(args) < 2:
+    print("Usage: python main.py <file>")
+    sys.exit(1)
+else:
+    file_name = (args[1])
+    if  file_name.endswith(".ai"):
+        content = getFileContent(file_name)
+        
+    
+    print(type(file_name))
+    
+               
